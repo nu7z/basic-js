8ecfdc259e45d9e06530fb96a5d8f9af546ed8be
Andrey <opexsomoff@gmail.com>
2020-10-31 07:47:48 +0900

    feat: add decision transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,20 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-    if (!Array.isArray(arr)) throw new Error('Error');
+    if (!Array.isArray(arr)) {
+        throw new Error();
+      }
     
-    const size = arr.length - 1;
-    const result = [];
-    const prevOrNext = {
-        prev: [`--double-prev`, `--discard-prev`],
-        next: [`--double-next`, `--discard-next`],
-
-    }
-
-    arr.forEach((item, index) => {
-        if (result.length === 0 && prevOrNext.prev.some((word) => word === item)) return;
-        if (index === size && prevOrNext.next.some((word) => word === item)) return;
-        result.push(item);
-    })
-    return result;
-};
+      const newArr = [];
+    
+      for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+          case "--discard-next":
+            newArr.push("");
+            i++;
+            break;
+          case "--discard-prev":
+            if (newArr.length !== 0) {
+              newArr.pop();
+            }
+            break;
+          case "--double-next":
+            if (i !== arr.length - 1) {
+              newArr.push(arr[i + 1]);
+            }
+            break;
+          case "--double-prev":
+            if (newArr.length !== 0) {
+              newArr.push(newArr[newArr.length - 1]);
+            }
+            break;
+          default:
+            newArr.push(arr[i]);
+        }
+      }
+    
+      return newArr.filter((e) => e !== "");
+}

8666f2da25a287b217c1227203d8ed592d2a6ac9
Andrey <opexsomoff@gmail.com>
2020-11-01 13:21:41 +0900

    feat: vineger-cliper decision

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,67 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(reverse = true) {
+    this.alphabet = "abcdefghijklmnopqrstuvwxyz";
+    this.isReverse = reverse;
+  }
+
+  encrypt(message, key) {
+    if (!message && !key) throw new Error("Error");
+
+    const alphabet = this.alphabet.split("");
+    let index = 0;
+    message = message.toLowerCase().split("");
+    key = key.toLowerCase().split("");
+
+    const getNewIndex = (e) => {
+      const newIndex = (alphabet.indexOf(e) + alphabet.indexOf(key[index])) % alphabet.length;
+      index === key.length - 1 ? (index = 0) : index++;
+
+      return newIndex;
+    }
+
+    
+    const encrypt = message
+      .reduce((a, e) => {
+        return alphabet.includes(e) ? (a += alphabet[getNewIndex(e)]) : (a += e);
+      }, "")
+      .toUpperCase();
+
+
+    if (!this.isReverse) {
+      return encrypt.split("").reverse().join("");
+    }
+
+    return encrypt;
+  }
+
+  decrypt(message, key) {
+    if (!message && !key) throw new Error("Error");
+
+    const alphabet = this.alphabet.split("");
+    let index = 0;
+    message = message.toLowerCase().split("");
+    key = key.toLowerCase().split("");
+
+    const getNewIndex = (e) => {
+      const newIndex =
+        (alphabet.indexOf(e) + alphabet.length - alphabet.indexOf(key[index])) % alphabet.length;
+      index === key.length - 1 ? (index = 0) : index++;
+      return newIndex;
+    }
+
+    const decrypt = message
+      .reduce((a, e) => {
+        return alphabet.includes(e) ? (a += alphabet[getNewIndex(e)]) : (a += e);
+      }, "")
+      .toUpperCase();
+
+    if (!this.isReverse) {
+      return decrypt.split("").reverse().join("");
+    }
+
+    return decrypt;
   }
 }
 
